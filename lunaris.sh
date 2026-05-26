219af059d9653e6a1e245355b8f3eb2cd761bd60
mdnoyon80123 <mdnoyon80123@gmail.com>
2026-05-26 20:19:33 +0600

    Initialize repo and build environment in lunaris.sh

--- lunaris.sh
+++ lunaris.sh
@@ -1,1 +1,14 @@
+#!/bin/bash
 
+rm -rf .repo/local_manifests
+
+repo init --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 16 -g default,-mips,-darwin,-notdefault
+
+git clone https://github.com/mdnoyon80123/hotdogb_local_manifest --depth 1 -b inf-q2 .repo/local_manifests
+
+/opt/crave/resync.sh
+
+source build/envsetup.sh
+lunch infinity_hotdogb-userdebug
+
+m bacon
